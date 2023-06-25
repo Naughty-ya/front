7252f615c2d01759ed944afbd4658c71b5fb7fc9
jeonbyeongmin <qudals7613@gmail.com>
2023-06-25 22:51:42 +0900

    feat: update kakao share

--- src/components/common/ShareButtons.tsx
+++ src/components/common/ShareButtons.tsx
@@ -1,31 +1,30 @@
 import { Flex } from 'src/components/core/Flex'
 import { IconButton } from 'src/components/core/IconButton'
-import { copyClipboard, shareKakaoLink } from 'src/utils/share'
+import { shareClipboard, shareKakaoLink, shareTwitter } from 'src/utils/share'
 
 export function ShareButtons() {
-  // TODO: 카카오 링크 공유하기, 트위터 공유하기, 링크 복사하기 기능 구현
-
   return (
     <Flex direction="row" className="gap-3" align="center">
       <IconButton
         iconName="kakao"
         id="kakao-link-btn"
         iconSize={22}
-        className="bg-yellow-400 text-gray-800 p-3 rounded-full drop-shadow-1"
+        className="bg-yellow-400 text-gray-800 p-3 rounded-full drop-shadow-1 hover:bg-yellow-500 active:bg-yellow-600 transition-all"
         onClick={shareKakaoLink}
       />
       <IconButton
         iconName="twitter"
         iconStroke="none"
         iconSize={22}
-        className="white bg-blue-400 p-3 rounded-full drop-shadow-1"
+        onClick={shareTwitter}
+        className="white bg-blue-400 p-3 rounded-full drop-shadow-1 hover:bg-blue-500 active:bg-blue-600 transition-all"
       />
       <IconButton
         iconName="link"
         iconFill="none"
         iconSize={22}
-        className="bg-brand-blue p-3 rounded-full drop-shadow-1"
-        onClick={() => copyClipboard(window.location.href)}
+        className="bg-blue-500 p-3 rounded-full drop-shadow-1 hover:bg-blue-600 active:bg-blue-700 transition-all"
+        onClick={() => shareClipboard(window.location.href)}
       />
     </Flex>
   )
--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,4 +1,4 @@
-export async function copyClipboard(text: string) {
+export async function shareClipboard(text: string) {
   try {
     await navigator.clipboard.writeText(text)
   } catch (err) {
@@ -15,12 +15,33 @@ export function shareKakaoLink() {
   }
 
   kakao.Share.sendDefault({
-    objectType: 'text',
-    text: '',
-    link: {
-      // TODO: url 수정
-      mobileWebUrl: 'https://example.com',
-      webUrl: 'https://example.com'
-    }
+    objectType: 'feed',
+    content: {
+      title: '너 T야?',
+      description: 'AI가 판별해주는 나의 T 성향',
+      imageUrl:
+        'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+      link: {
+        webUrl: import.meta.env.VITE_KAKAO_SHARE_URL
+      }
+    },
+    buttons: [
+      {
+        title: '웹으로 보기',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com'
+        }
+      },
+      {
+        title: '앱으로 보기',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com'
+        }
+      }
+    ]
   })
 }
+
+export function shareTwitter() {}
