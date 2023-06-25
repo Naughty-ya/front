6d75ce8a2439f1e84282c0aab610ecabd47612f4
Jxxunnn <roof2586@naver.com>
2023-06-25 17:08:58 +0900

    feat: chat 작업중
    Co-authored-by: Jinyoung C <yoooooooung@users.noreply.github.com>
    Co-authored-by: LJJ <wjdrk70@users.noreply.github.com>
    Co-authored-by: yoonsooho <yoonsooho@users.noreply.github.com>
    Co-authored-by: Byeongmin Jeon <qudals7613@gmail.com>

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -12,6 +12,8 @@ import reactIcon from 'src/assets/react.svg'
 import send from 'src/assets/icons/send.svg'
 import spinnerIcon from 'src/assets/icons/spinner.svg'
 import { FetchClient } from 'src/api'
+import { useQuery } from 'src/hooks/use-query'
+import { useNavigate } from 'react-router-dom'
 
 type TMessageListItem = {
   question: string
@@ -25,6 +27,13 @@ const TOTAL = 10
 export default function Chat() {
   const [userAnswer, setUserAnswer] = useState('')
   const [qnaList, setQnaList] = useState([questions[0].question])
+  const [isLoading, setLsLoading] = useState(false)
+  const nickname = useQuery().get('nickname')
+  const navigate = useNavigate()
+
+  // navigate('/', { list: '리스트' })
+
+  /*  <Link to="new-path" state={{ some: "value" }} /> */
 
   const handleChangeUserAnswer = (e: ChangeEvent<HTMLInputElement>) => {
     setUserAnswer(e.target.value)
@@ -38,6 +47,7 @@ export default function Chat() {
     setQnaList([...qnaList, userAnswer, questions[index.current].question])
     setUserAnswer('')
     index.current += 1
+
     return
   }
 
@@ -55,38 +65,62 @@ export default function Chat() {
     return list
   }, [qnaList])
 
-  const handleSubmitChatResult = (resultData: TMessageListItem[]) => {
-    //대충 api 요청
-    new FetchClient().post('http://localhost:3000/openai', { list: resultData })
+  /* 
+  1. 최종 버튼을 누르면 api 함수를 실행한다.
+  2. 로딩 UI가 표시된다. 2초동안.
+  3. response값을 가지고 result 페이지로 라우팅한다.
+*/
+
+  const handleSubmitChatResult = async (resultData: TMessageListItem[]) => {
+    try {
+      const data = await new FetchClient().post(
+        'http://localhost:3000/openai',
+        {
+          list: resultData
+        }
+      )
+      navigate('/result', data)
+      setLsLoading(false)
+    } catch (e) {
+      setLsLoading(false)
+      throw Error('fetch failed')
+    } finally {
+      navigate('/result')
+    }
   }
+
   const backAnswerHandler = () => {
-    const copy = [...qnaList]
-    copy.splice(copy.length - 2)
-    setQnaList(copy)
-    // setQnaList([...qnaList].splice(qnaList.length - 2))
+    setQnaList([...qnaList].slice(0, qnaList.length - 2))
     index.current -= 1
   }
 
   return (
-    <div id="chat-wrapper">
-      <ChatHeader className="flex items-center py-6 bg-black">
-        <ChatHeader.BackButton onClick={() => {}} />
-        <ChatHeader.Title className="flex-1 mx-auto text-xl font-bold">
+    <div id="chat-wrapper px-2 relative">
+      <ChatHeader className="fixed w-full py-4 bg-black max-w-[500px]">
+        <ChatHeader.BackButton
+          className="absolute left-6"
+          onClick={() => {
+            navigate('/')
+          }}
+        />
+        <ChatHeader.Title className="text-xl font-bold text-center ">
           너 T야?
         </ChatHeader.Title>
       </ChatHeader>
 
       <div
         id="chat-message-wrapper"
-        className="p-5 text-black bg-black h-[calc(100vh-216px)] overflow-y-scroll"
+        className="p-5 text-black h-[calc(100vh-120px)] overflow-auto max-w-[500px] pt-20"
       >
         <ChatMessage>
           <div id="chat-message-default-system" className="flex flex-col">
-            <div className="flex items-center gap-1 mb-3">
+            <div className="flex items-center gap-2 mb-3">
               <ChatMessage.Avatar src={reactIcon} />
               <ChatMessage.SenderName />
             </div>
-            <ChatMessage.Bubble type="system">안녕 쿠키</ChatMessage.Bubble>
+            <ChatMessage.Bubble type="system">
+              안녕 {nickname}
+            </ChatMessage.Bubble>
             <ChatMessage.Bubble type="system">
               지금부터 내가 5가지 질문을 할 거야
             </ChatMessage.Bubble>
@@ -103,7 +137,7 @@ export default function Chat() {
         </ChatMessage>
         <ChatMessage>
           <div className="flex flex-col">
-            <div className="flex items-center gap-1 mb-3">
+            <div className="flex items-center gap-2 mb-3">
               <ChatMessage.Avatar src={reactIcon} />
               <ChatMessage.SenderName />
             </div>
@@ -121,7 +155,7 @@ export default function Chat() {
                   {qnaList.length - 1 === idx && qnaList.length !== 1 && (
                     <ChatMessage.UndoButton
                       onClick={backAnswerHandler}
-                      className="text-amber-600"
+                      className="text-white"
                     />
                   )}
                 </React.Fragment>
@@ -131,20 +165,24 @@ export default function Chat() {
         </ChatMessage>
       </div>
       {qnaList.length >= 10 ? (
-        <ChatSubmitButton
-          onClick={() => {
-            handleSubmitChatResult(resultData)
-          }}
-        />
+        <div className="mx-5">
+          <ChatSubmitButton
+            onClick={() => {
+              handleSubmitChatResult(resultData)
+            }}
+            className="w-full px-8 py-2 my-5 text-lg font-semibold text-white rounded bg-gra drop-shadow-1"
+          />
+          {/*  */}
+          {/* */}
+        </div>
       ) : (
         <form
           onClick={e => {
             e.preventDefault()
           }}
           id="chat-form"
-          className="mt-10"
         >
-          <ChatChoice className="flex gap-3 mb-3.5 w-screen overflow-x-scroll">
+          <ChatChoice className="flex gap-3 mb-3.5 w-screen overflow-auto max-w-[500px] px-5">
             {questions.map((q, idx) => {
               if (MAX_QNA_LENGTH < index.current && idx === MAX_QNA_LENGTH)
                 return null
@@ -167,9 +205,9 @@ export default function Chat() {
               )
             })}
           </ChatChoice>
-          <div className="flex">
+          <div className="flex max-w-[500px] px-5">
             <ChatInput
-              className="text-red-800 rounded py-2 px-3.5 w-full mr-2"
+              className="text-white rounded py-2 px-3.5 w-full mr-2"
               value={userAnswer}
               maxLength={50}
               onChange={handleChangeUserAnswer}
@@ -185,7 +223,7 @@ export default function Chat() {
           </div>
         </form>
       )}
-      {false && (
+      {isLoading && (
         <ChatLoading className="absolute inset-0 z-50 flex flex-col items-center justify-center gap-4 bg-blue-100 pointer-evente-none opacity-80">
           <img src={spinnerIcon} alt="로딩 중" className="animate-spin" />
           <p>잠시 기다려줘 쿠키</p>
