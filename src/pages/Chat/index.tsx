a7d8b558d4cec2f897f35099ac2268316178de7e
LJJ <LJJ@LJJ.local>
2023-06-26 03:06:22 +0900

    set up proxy url

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -108,7 +108,7 @@ export default function Chat() {
     setLsLoading(true)
     try {
       const data = await new FetchClient().post(
-        'https://36naf0ynve.execute-api.ap-northeast-2.amazonaws.com/default/naughty-ya/',
+        'https://ethssfzmm3.execute-api.ap-northeast-2.amazonaws.com/default/naughty-ya',
         {
           list: resultData
         }
