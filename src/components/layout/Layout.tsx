8918d3fec7c0fb2f1c9ef390fe506f558273a667
LJJ <LJJ@LJJ.local>
2023-06-27 00:05:03 +0900

    scroll update

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,12 +3,12 @@ import bg from 'src/assets/img/bg.webp'
 export function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
     <div
-      className="bg-cover bg-center w-full flex-1 max-h-[1000px] max-w-[500px] overflow-auto flex items-center overscroll-contain touch-none"
+      className="bg-cover bg-center w-full flex-1 max-h-[1000px] max-w-[500px] overflow-auto flex items-center"
       style={{
         backgroundImage: `url(${bg})`
       }}
     >
-      <div className="m-auto py-10 w-full">{children}</div>
+      <div className="w-full py-10 m-auto">{children}</div>
     </div>
   )
 }
