110641c7735eb5227008bcf83b99220cf76ed099
LJJ <LJJ@LJJ.local>
2023-06-26 17:35:43 +0900

    17:35

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -108,7 +108,7 @@ export default function Chat() {
     setLsLoading(true)
     try {
       const data = await new FetchClient().post(
-        `${process.env.BASE_URL}/api/openai`,
+        `${import.meta.env.BASE_URL}/api/openai`,
         {
           list: resultData
         }
