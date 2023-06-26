db8487efe6a359391e3070a85d8321353b671d77
jeonbyeongmin <qudals7613@gmail.com>
2023-06-26 17:26:07 +0900

    feat: add toast message

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import './assets/fonts/dunggeunmo/dunggeunmo.css'
 import './assets/fonts/pretendard/pretendardvariable.css'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
   </BrowserRouter>
 )
